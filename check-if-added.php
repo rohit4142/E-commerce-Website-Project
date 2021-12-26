82b315763e959f324a0716e418b1818ed7c2309a
Rohit Kumar Jaiswal <89607500+rohit4142@users.noreply.github.com>
2021-12-27 03:47:09 +0530

    Create check-if-added.php

---
+++ check-if-added.php
@@ -0,0 +1,19 @@
+<?php
+
+
+function check_if_added_to_cart($item_id) {
+    $user_id = $_SESSION['user_id']; //We'll get the user_id from the session
+    require("common.php"); // connecting to the database
+    
+    
+    $query = "SELECT * FROM users_items WHERE item_id='$item_id' AND user_id ='$user_id' and status='Added to cart'";
+    $result = mysqli_query($con, $query) or die(mysqli_error($con));
+    
+
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+?>
